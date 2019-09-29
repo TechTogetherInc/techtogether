e710b059c53557452d82d2414541745c67940514
NaginiChen <naginichen@gmail.com>
2019-09-29 14:12:32 -0400

    Home Page Update
    Sass added, html changed, svg of TT logo added, some mobile respnsivness added. Home page's main styles is coming from "main.css/main.csss" instead of "style.css"

---
+++ js/circle.js
@@ -0,0 +1,9 @@
+var cw = $('.height-change-js').width();
+$('.height-change-js').css({'height': cw+'px'});
+
+var cw2 = $('.height-change-js-2').width();
+$('.height-change-js-2').css({'height': cw2+'px'});
+
+var cw3 = $('.height-change-js-3').width();
+$('.height-change-js-3').css({'height': cw3+'px'});
+
