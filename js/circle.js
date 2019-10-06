dbaa7cb72ec3ab82fc41acbe5fbc39ece5e8a0fc
NaginiChen <naginichen@gmail.com>
2019-10-06 03:25:11 -0400

    Dynamic circles, color chaning circles when hovered

--- js/circle.js
+++ js/circle.js
@@ -1,9 +1,25 @@
+// makes the circles on the home page have equal width and height no matter the screen size
+
+// On load
 var cw = $('.height-change-js').width();
-$('.height-change-js').css({'height': cw+'px'});
+$('.height-change-js').css({ 'height': cw + 'px' });
 
 var cw2 = $('.height-change-js-2').width();
-$('.height-change-js-2').css({'height': cw2+'px'});
+$('.height-change-js-2').css({ 'height': cw2 + 'px' });
 
 var cw3 = $('.height-change-js-3').width();
-$('.height-change-js-3').css({'height': cw3+'px'});
+$('.height-change-js-3').css({ 'height': cw3 + 'px' });
+
+
+// When screen size changes
+$(window).resize(function () {
+    var cw = $('.height-change-js').width();
+    $('.height-change-js').css({ 'height': cw + 'px' });
+
+    var cw2 = $('.height-change-js-2').width();
+    $('.height-change-js-2').css({ 'height': cw2 + 'px' });
+
+    var cw3 = $('.height-change-js-3').width();
+    $('.height-change-js-3').css({ 'height': cw3 + 'px' });
+});
 
