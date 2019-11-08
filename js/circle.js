89510c6d71620fef5ffa91559b5ffb3415f7c508
fionawhittington <fionaw@bu.edu>
2019-11-08 15:04:26 -0500

    update styling

--- js/circle.js
+++ js/circle.js
@@ -23,3 +23,54 @@ $(window).resize(function () {
     $('.height-change-js-3').css({ 'height': cw3 + 'px' });
 });
 
+var current = 1;
+var max = $( "li" ).length +1;
+
+$(".slidercontent").hide();
+$(".slidercontent:nth-child(1)").fadeIn("slow");
+
+//function to change to next quote
+function changeUp() {
+  $(".slidercontent").hide();
+  current += 1;
+  if (current === max) {
+    current = 1;
+  }
+$(".slidercontent:nth-child(" + current + ")").fadeIn("slow");
+}
+
+function changeDown() {
+  $(".slidercontent").hide();
+  current -= 1;
+
+  if (current === 0) {
+    current = max - 1;
+  }
+
+  $(".slidercontent:nth-child(" + current + ")").fadeIn("slow");
+}
+
+startChange();
+
+$(".sliderspot2").click(function() { 
+  stopChange();
+  changeUp();
+  startChange();
+});
+
+$(".sliderspot").click(function() {
+  stopChange();
+  changeDown();
+  startChange();
+});
+
+//FUNCTIONS TO CONTROL TIMING CHANGES
+function startChange() {
+changeIt = setInterval(function(){ 
+changeUp();
+}, 10000);
+};
+
+function stopChange() {
+    clearInterval(changeIt);
+}
